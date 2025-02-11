dfa0042ab66aead84ab80cbe7a1fefc0e7080e5f
Madhavi Palla <madhavi.palla09@gmail.com>
2025-02-11 18:11:02 +0900

    Add files via upload

---
+++ task-management-app/backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.config');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/tasks', require('./routes/task.routes'));
+app.use('/api/users', require('./routes/user.routes'));
+
+app.get('', (req, res) => {
+    res.send('API is running....');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
